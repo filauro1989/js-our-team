f9787ddbbe0baabe747ed2c19e81807a64ca1980
filauro1989 <filauro1989@gmail.com>
2021-12-09 14:47:20 +0100

    done

--- js/script.js
+++ js/script.js
@@ -30,3 +30,67 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+// esplicito il container 
+const teamContainer = document.querySelector('.team-container');
+
+// creo funzione per inserire le card
+function printCards(array, container) {
+  // svuoto il container
+  container.innerHTML = '';
+
+  // ciclo l'array per selezionare i dati
+  for (let i = 0; i < array.length; i++) {
+
+    // assegno variabile obj ad ogni elemento dell'array
+    const obj = array[i];
+
+    // assegno variabile con dati da stampare
+    const templateDiv = `
+      <div class="team-card">
+        <div class="card-image">
+          <img
+            src="img/${obj.image}"
+            alt="${obj.name}"
+          />
+        </div>
+        <div class="card-text">
+          <h3>${obj.name}</h3>
+          <p>${obj.role}</p>
+        </div>
+      </div>`;
+
+    // stampo nel container i dati
+    container.innerHTML += templateDiv;
+  }
+}
+
+// richiamo la funzione
+printCards(team, teamContainer);
+
+// assegno variabili ad elementi del dom
+let inputName = document.getElementById('name');
+let inputRole = document.getElementById('role');
+let inputButton = document.getElementById('addMemberButton');
+
+// aggiungo event listener al button 
+inputButton.addEventListener('click', function(){
+
+  // prelevo i valori dai campi nome e ruolo
+  let newName = inputName.value;
+  let newRole = inputRole.value;
+
+  // assegno variabile ai valori prelevati
+  let obj = {
+    name: newName,
+    role: newRole,
+    image: 'new-team-member-03.jpg',
+  }
+
+  // pusho i valori dentro l'array
+  team.push(obj);
+
+  // richiamo la funzione che pulisce il dom e lo ristampa con i nuovi dati
+  printCards(team, teamContainer);
+
+});
